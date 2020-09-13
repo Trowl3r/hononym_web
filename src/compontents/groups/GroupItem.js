17f13b72877dd51641c81757f9f15a430336994b
Torben <Riaslike@gmail.com>
2020-09-13 20:08:35 +0200

    Made the Group Item and started with the Profile Component (Styling)

--- src/compontents/groups/GroupItem.js
+++ src/compontents/groups/GroupItem.js
@@ -1,23 +1,58 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const GroupItem = ({group: {name, desc, members, follower, posts, _id, groupImage}}) => {
-    return (
-        <div>
-            <img src={`http://localhost:5000/${groupImage}`} />
-            <h2>{name}</h2>
-            <h2>{desc}</h2>
-            <h2>Members: {members.length}</h2>
-            <h2>follwer: {follower.length}</h2>
-            <h2>posts: {posts.length}</h2>
-            <Link to={`/group/${_id}`} >View Group</Link>
-        </div>
-    )
+//Material UI
+import { Avatar, CardHeader, Container, Card, Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  cardClass: {
+    borderRadius: "0px",
+  },
+  large: {
+    width: theme.spacing(7),
+    height: theme.spacing(7),
+  },
+  action: {
+    alignSelf: "center",
+  },
+}));
+
+const GroupItem = ({
+  group: { name, desc, members, follower, posts, _id, groupImage },
+}) => {
+  const classes = useStyles();
+
+  return (
+    <Container component="main" maxWidth="md">
+      <Card classes={{ root: classes.cardClass }}>
+        <CardHeader
+          avatar={
+            <Avatar
+              src={`http://localhost:5000/${groupImage}`}
+              alt={name}
+              className={classes.large}
+            />
+          }
+          classes={{ action: classes.action }}
+          
+          action={
+            <Button href={`/group/${_id}`} color="primary" variant="contained">Go to Group</Button>
+          }
+
+          titleTypographyProps={{variant: 'h6', color: "textPrimary"}}
+          title={name}
+          subheaderTypographyProps={{variant: 'p'}}
+          subheader={desc}
+        />
+      </Card>
+    </Container>
+  );
 };
 
 GroupItem.propTypes = {
-    group: PropTypes.object.isRequired,
-}
+  group: PropTypes.object.isRequired,
+};
 
 export default GroupItem;
--- src/compontents/profiles/Profile.js
+++ src/compontents/profiles/Profile.js
@@ -3,13 +3,36 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
 import { getProfileById } from "../../actions/profile";
-import ProfileTop from "./ProfileTop";
 import ProfileFollowing from "./ProfileFollowing";
+
+//Material UI
+import { Avatar, CardHeader, Container, Card } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  large: {
+    width: theme.spacing(7),
+    height: theme.spacing(7),
+  },
+  linkColor: {
+    color: "#9E9E9E",
+    textDecoration: "none",
+  },
+  linkColorName: {},
+  action: {
+    alignSelf: "center",
+  },
+  cardClass: {
+    borderRadius: "0px",
+  },
+}));
 
 const Profile = ({ getProfileById, profile: { profile }, auth, match }) => {
   useEffect(() => {
     getProfileById(match.params.id);
   }, [getProfileById, match.params.id]);
+
+  const classes = useStyles();
 
   return (
     <Fragment>
@@ -17,9 +40,25 @@ const Profile = ({ getProfileById, profile: { profile }, auth, match }) => {
         <Spinner />
       ) : (
         <Fragment>
-          <h1>Profile</h1>
-          <ProfileTop profile={profile} />
-          <ProfileFollowing profile={profile}/>
+          <Container component="main" maxWidth="md">
+            <Card>
+              <CardHeader
+                avatar={
+                  <Avatar
+                    src={`http://localhost:5000/${profile.profileImage}`} alt={profile.name} className={classes.large}
+                  />
+                }
+                action={<ProfileFollowing profile={profile} />}
+
+                classes={{ action: classes.action }}
+
+                titleTypographyProps={{variant: 'h6', color: "textPrimary"}}
+                title={profile.name}
+                subheaderTypographyProps={{variant: 'p'}}
+                subheader={`@${profile.user.username}`}
+              />
+            </Card>
+          </Container>
         </Fragment>
       )}
     </Fragment>
